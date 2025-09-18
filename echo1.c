1da472bf61c31d34bd1961b1db5e8ad14e20c60b
JD-D3V <millsjd@telus.net>
2025-09-18 23:25:14 +0000

    message

--- echo1.c
+++ echo1.c
@@ -1,7 +1,19 @@
 #include <unistd.h>
-#include <stdio.h>
+#define CMAX 300
 
-int main() {
-    write(1, "Hello, World!\n", 15);
-    return 0;
+int main(void) {
+    char buffer[CMAX], c; 
+    int i, n;
+
+    while (1) {
+        write(1, "$ ", 2);
+        i = 0;
+        n = read(0, &c, 1);
+        while (n > 0 && c != '\n' && i < CMAX - 1) {
+            buffer[i++] = c;
+            n = read(0, &c, 1);
+        }
+        write(1, buffer, i);        
+        write(1, "\n", 1);  
+    }
 }
